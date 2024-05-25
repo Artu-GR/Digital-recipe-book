93a92e54f580cc5cf401501dadddeb6f9b88a655
Artu-GR <arturo.drago@gmail.com>
2024-05-25 09:17:43 -0600

    Redesign

--- PIA-AppMoviles/src/app/spoonacular.service.ts
+++ PIA-AppMoviles/src/app/spoonacular.service.ts
@@ -10,12 +10,12 @@ export class SpoonacularService {
   constructor(private httpClient: HttpClient) { }
 
   //apiKey: string = '73bf64a5f64240ea9e7564f169b67705';
-  //apiKey: string = '28e40e19999d48c7ad570f84cc2987ff';
+  apiKey: string = '28e40e19999d48c7ad570f84cc2987ff';
   //apiKey: string = '75a888f6bda74747bcce5dd958d762b5';
   //apiKey: string = '3880af13ea794cf19e0c6c9f69d45af4';
   //apiKey: string = 'ff39dbe9c4094b6fbb209398b4e908b2';
   //apiKey: string = '3815a1984c9f43fd9a6a52fdb4fbf2a0';
-  apiKey: string = '593e4ce868d04326ad4873d872e3ebfb';
+  //apiKey: string = '593e4ce868d04326ad4873d872e3ebfb';
 
   searchRecipes(query: string): Observable<any>{
     return this.httpClient.get(`https://api.spoonacular.com/recipes/search?query=${query}&apiKey=${this.apiKey}`);
